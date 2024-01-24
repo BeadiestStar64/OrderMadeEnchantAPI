8a410f57682bf99c4835bbe4df9c60dd14313fe3
BeadiestStar64 <beadiestbench64@gmail.com>
2024-01-24 09:07:19 +0900

    first commit

---
+++ src/main/java/com/github/BeadiestStar64/Plugin/OrderMade.java
@@ -0,0 +1,8 @@
+package com.github.BeadiestStar64.Plugin;
+
+public interface OrderMade {
+
+    String getNameSpace();
+    String getDisplayName();
+    String getLevelLore();
+}
---
+++ src/main/java/com/github/BeadiestStar64/Plugin/OrderMadeType.java
@@ -0,0 +1,19 @@
+package com.github.BeadiestStar64.Plugin;
+
+public abstract class OrderMadeType implements OrderMade {
+
+    @Override
+    public String getNameSpace() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getLevelLore() {
+        return null;
+    }
+}
